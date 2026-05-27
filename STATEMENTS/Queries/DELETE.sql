12b252371a26408ede0611b69cac9900d6c06cd7
Samuel Ahumada <samuel.ahumada-f@mail.escuelaing.edu.co>
2026-05-27 17:08:20 -0500

    Commit 2

---
+++ STATEMENTS/Queries/DELETE.sql
@@ -0,0 +1,7 @@
+DELETE 
+FROM table1
+
+/*
+DELETE es una QUERY mediante la cual se pueden borrar filas de una tabla.
+Se puede filtrar mediante el uso de CLAUSES, si no se especifica nada, se borran todas las filas de la tabla.
+*/
--- STATEMENTS/Queries/UPDATE.sql
+++ STATEMENTS/Queries/UPDATE.sql
@@ -1,3 +1,8 @@
 UPDATE table_1
-SET column1 = value1, column2 = value2,... 
-WHERE condition;
+SET column1 = value1, column2 = value2, ... , column_n = value_n
+
+/*
+UPDATE es una QUERY la cual permite actualizar las filas de una tabla.
+Esta se complementa con SET y con CLAUSES para poder filtrar que filas en especifico se quieren cambiar.
+Si no se hacer filtros se cambiaran todos los valores de la tabla, por eso es recomendable usarlas con TRANSACTION.
+*/

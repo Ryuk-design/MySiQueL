8ef35452cc6ae93bf4bce747cc00b86399f76972
Samuel Ahumada <samuel.ahumada-f@mail.escuelaing.edu.co>
2026-06-01 22:42:11 -0500

    Actualizacion en reglas de uso de las clauses UPDATE y DELETE

--- STATEMENTS/Queries/DELETE.sql
+++ STATEMENTS/Queries/DELETE.sql
@@ -3,5 +3,7 @@ FROM table1
 
 /*
 DELETE es una QUERY mediante la cual se pueden borrar filas de una tabla.
-Se puede filtrar mediante el uso de CLAUSES, si no se especifica nada, se borran todas las filas de la tabla.
+Se puede filtrar mediante el uso de JOIN, WHERE Y ORDER BY + LIMIT.
+No se puede utilizar clauses distintas a las mencionadas anteriormente de manera directa, se pueden usar en subqueries.
+Si no se especifica nada, se borran todas las filas de la tabla.
 */
--- STATEMENTS/Queries/UPDATE.sql
+++ STATEMENTS/Queries/UPDATE.sql
@@ -3,6 +3,7 @@ SET column1 = value1, column2 = value2, ... , column_n = value_n
 
 /*
 UPDATE es una QUERY la cual permite actualizar las filas de una tabla.
-Esta se complementa con SET y con CLAUSES para poder filtrar que filas en especifico se quieren cambiar.
+Esta se complementa con SET, JOINS y WHERE para poder filtrar que filas en especifico se quieren cambiar.
+No se puede utilizar clauses distintas a las mencionadas anteriormente de manera directa, se pueden usar en subqueries.
 Si no se hacer filtros se cambiaran todos los valores de la tabla, por eso es recomendable usarlas con TRANSACTION.
 */

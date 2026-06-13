a69f4910c20c2507aa9bcecfab69769880521aa0
Samuel Ahumada <samuel.ahumada-f@mail.escuelaing.edu.co>
2026-06-13 15:49:26 -0500

    Commit 12

---
+++ STATEMENTS/Funciones rebuscadisimas/DATEDIFF.sql
@@ -0,0 +1,6 @@
+DATEDIFF(fecha1, fecha2)
+/*
+Calcula la diferencia entre dos dias
+Se puede utilizar en problemas que requieran comparacion de fechas exactamente anteriores
+por ejemplo comparar los datos de hoy con los datos de ayer, mas no el dato anterior
+*/
--- STATEMENTS/Queries/DELETE.sql
+++ STATEMENTS/Queries/DELETE.sql
@@ -6,4 +6,5 @@ DELETE es una QUERY mediante la cual se pueden borrar filas de una tabla.
 Se puede filtrar mediante el uso de JOIN, WHERE Y ORDER BY + LIMIT.
 No se puede utilizar clauses distintas a las mencionadas anteriormente de manera directa, se pueden usar en subqueries.
 Si no se especifica nada, se borran todas las filas de la tabla.
+Si se utilizan JOINS despues de DELETE se debe especificar la tabla de la que se van a eliminar los datos
 */
